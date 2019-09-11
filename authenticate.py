37c1e3a7fd2b50a270d011a7cb869b1d0eba4470
Sara Rey <sara.rey.manchester@gmail.com>
2019-09-11 15:12:15 +0100

    authentication methods added

--- authenticate.py
+++ authenticate.py
@@ -1,6 +1,7 @@
 # authenticate to obtain access token with write permissions
 import requests
 import json
+import time
 
 from config import v1_api
 from config import v2_api
@@ -24,18 +25,51 @@ def load_config_file(pth):
 def get_verification_device_code(client_id, requested_scope, resource):
     if resource != None: # Handle this better in production
         requested_scope = f"{requested_scope} {resource}"
-    files = None
-    url = f"{v1_api}/oauthv2/deviceauthorization"
+    device_code = None
+    url = "https://api.basespace.illumina.com/v1pre3/oauthv2/deviceauthorization"
     p = {"client_id": client_id, "response_type": "device_code", "scope": requested_scope}
     response = requests.post(url, params=p)
-    print(response.url)
     if response.status_code != 200:
         print("error")
         print(response.status_code)
         print(response.text)
     else:
-        files = response.json()
-    return files
+        print(response.json())
+        link = response.json().get("verification_with_code_uri")
+        print(f"Please open this link and authorise the app: {link}")
+        device_code = response.json().get("device_code")
+    return device_code
+
+
+def poll_for_access_token(config, device_code):
+    token = None
+    iterate = True
+    while iterate:
+        url = "https://api.basespace.illumina.com/v1pre3/oauthv2/token"
+        p = {"client_id": config.get("clientId"), "client_secret": config.get("clientSecret"),
+             "code": device_code, "grant_type":"device"}
+        response = requests.post(url, params=p)
+        if response.status_code != 200 and response.status_code != 400:
+            print("error")
+            print(response.status_code)
+            print(response.text)
+        elif response.status_code == 200:
+            token = response.json()
+            iterate = False
+        else:
+            print(response.json())
+            time.sleep(0.7)
+    return token
+
+
+def update_config_file(pth, access_token):
+    with open(pth + "bs.config.json") as config_file:
+        try:
+            config_json = json.load(config_file)
+            config_json["authenticationToken"] = access_token
+        except json.decoder.JSONDecodeError:
+            raise Exception("Config file does not contain valid json")
+    return config_json
 
 
 def main():
@@ -44,7 +78,10 @@ def main():
     auth = 'Bearer ' + configs.get("authenticationToken")
 
     # Start here for new authentication workflow
-    print(get_verification_device_code(configs.get("clientId"), "create global", None))
+    code = get_verification_device_code(configs.get("clientId"), "create global", None)
+    token = poll_for_access_token(configs, code)
+
+    update_config_file(config_file_pth, token)
 
 
 if __name__ == '__main__':
